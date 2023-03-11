9184f9af2a3b937fe7c40ed5fa17011e9cba6c24
Madipta <madipta@gmail.com>
2023-03-11 17:32:07 +0700

    nx upgrade 15.8.6

--- libs/services/jest.config.ts
+++ libs/services/jest.config.ts
@@ -2,12 +2,10 @@
 export default {
   displayName: 'services',
   preset: '../../jest.preset.js',
-  globals: {
-    'ts-jest': { tsconfig: '<rootDir>/tsconfig.spec.json' },
-  },
+  globals: {},
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest',
+    '^.+\\.[tj]sx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/libs/services',
